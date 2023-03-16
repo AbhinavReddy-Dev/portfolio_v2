a701b83cc0838e7b69887f3c918c895f99334efc
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-03-16 01:09:43 -0700

    added projects data and posts data using glob

---
+++ src/data/projects.ts
@@ -0,0 +1,12 @@
+export const projects = [
+  {
+    id: 1,
+    name: "Frontend Mentor Challenges",
+    description:
+      "A collection of challenges from Frontend Mentor to practice my skills. I'm using this as a way to learn new technologies and improve my frontend skills, I will be adding more projects as I complete them.",
+    url: "",
+    github: "",
+    image: "",
+    technologies: ["HTML", "CSS", "TypeScript", "Astro"],
+  },
+];
