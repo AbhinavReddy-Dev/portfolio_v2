59ec72c46436b3c7e4d716d7141a3ff64087c77d
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-03-16 14:40:31 -0700

    fixed posts slug error during build

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,8 +4,8 @@ export const projects = [
     name: "Frontend Mentor Challenges",
     description:
       "A collection of challenges from Frontend Mentor to practice my skills. I'm using this as a way to learn new technologies and improve my frontend skills, I will be adding more projects as I complete them.",
-    url: "",
-    github: "",
+    url: "https://frontendwizard.netlify.app/",
+    github: "https://github.com/AbhinavReddy-Dev/frontendmentor",
     image: "",
     technologies: ["HTML", "CSS", "TypeScript", "Astro"],
   },
