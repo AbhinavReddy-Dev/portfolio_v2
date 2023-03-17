dcf6cd66f46698055a92280ed71d310ef8508104
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-03-17 01:31:09 -0700

    added hamburger menu fucntionality

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,12 +1,13 @@
 export const projects = [
   {
     id: 1,
-    name: "Frontend Mentor Challenges",
+    name: "Frontend Mentor ",
     description:
       "A collection of challenges from Frontend Mentor to practice my skills. I'm using this as a way to learn new technologies and improve my frontend skills, I will be adding more projects as I complete them.",
     url: "https://frontendwizard.netlify.app/",
     github: "https://github.com/AbhinavReddy-Dev/frontendmentor",
     image: "",
     technologies: ["HTML", "CSS", "TypeScript", "Astro"],
+    date: "03-16-2023",
   },
 ];
