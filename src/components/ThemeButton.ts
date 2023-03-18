d5498d61eeab56975b0003633d20d39ceccb9269
AbhinavReddy-Dev <abhinavreddy9681@gmail.com>
2023-03-17 18:51:30 -0700

    slight modificatoins to nav bar

--- src/components/ThemeButton.ts
+++ src/components/ThemeButton.ts
@@ -1,5 +1,5 @@
 // Theme button logic
-// const theme_button = document.querySelector<HTMLButtonElement>("#theme_button");
+
 const light_theme = document.querySelectorAll<HTMLSpanElement>("#light_theme");
 const dark_theme = document.querySelectorAll<HTMLSpanElement>("#dark_theme");
 const theme = (() => {
@@ -19,11 +19,6 @@ const set_dark_theme = () => {
   dark_theme?.forEach((e) => {
     e.classList.add("bg-blue-300");
   });
-
-  // light_theme?.classList.remove("bg-violet-400");
-  // light_theme.classList.remove("border-black");
-  // dark_theme?.classList.add("bg-blue-300");
-  // dark_theme.classList.add("border-white");
   document.documentElement.classList.add("dark");
   window.localStorage.setItem("theme", "dark");
 };
@@ -36,10 +31,6 @@ const set_light_theme = () => {
     e.classList.remove("bg-blue-300");
   });
 
-  // dark_theme?.classList.remove("bg-blue-300");
-  // dark_theme.classList.remove("border-white");
-  // light_theme?.classList.add("bg-violet-400");
-  // light_theme.classList.add("border-black");
   window.localStorage.setItem("theme", "light");
   document.documentElement.classList.remove("dark");
 };
@@ -57,16 +48,6 @@ dark_theme?.forEach((e) => {
     set_dark_theme();
   });
 });
-
-// light_theme?.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   set_light_theme();
-// });
-// dark_theme?.addEventListener("click", (e) => {
-//   console.log("dt click");
-//   e.preventDefault();
-//   set_dark_theme();
-// });
 
 if (theme === "light") {
   set_light_theme();
